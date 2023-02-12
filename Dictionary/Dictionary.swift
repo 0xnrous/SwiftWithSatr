42b9732f008fbccefbc25f4e0ae9a4b14b81f1d2
Mohamed Abdulaziz Eisa <83542891+0xnrous@users.noreply.github.com>
2023-02-12 15:52:41 +0200

    Add files via upload

---
+++ Dictionary/Dictionary.swift
@@ -0,0 +1,18 @@
+var score = ["Eisa": 100 , "Ragab": 100 , "Gerges": 200]
+print (score)
+
+// score ["Eisa"] = 300
+// print(score)
+// score.updateValue(400, forKey: "Ragab")
+// print (score)
+
+// // to delete value from dictionary 
+
+// score ["Eisa"]=nil
+// print (score)
+// var boolscore = score.isEmpty
+
+ // var httpStatusCodes = [200: "Success",
+ // 302: "Temporary Redirect",
+ // 404: "Not Found"]
+ // httpStatusCodes[503] = "Service Unavailable"
