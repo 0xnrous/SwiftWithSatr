e8937bb089d6a2a2d67ef68ac43b6ca38c6fa0c0
Mohamed Abdulaziz Eisa <83542891+0xnrous@users.noreply.github.com>
2023-02-13 13:20:35 +0200

    Add files via upload

---
+++ Function /Coffe_color/Coffe.color.swift
@@ -0,0 +1,19 @@
+// make arabic coffe example 
+// func makearabicofee(){
+//                 print ("step 1")
+//                 print ("step 2")
+//                 print ("step 3")
+//                 print ("step 4")
+//                 print ("step 5")
+
+// }
+// makearabicofee()
+
+// example for MyfavColor
+
+func MyfavColor (_ mycolor: String){
+
+        print ("my Fav color is \(mycolor)")
+}
+MyfavColor("green")
+MyfavColor("white")
---
+++ Function/example1.swift
@@ -0,0 +1,23 @@
+// this function example for adattion two number
+
+// func addition (number1 : Int , number2 : Int ){
+//         let result = number1 + number2 
+//         print (result)
+// }
+// addition(number1: 5, number2: 6)
+
+
+// function example using return 
+
+// func addition (number1 : Int , number2 : Int ) -> Int{
+//         let result = number1 + number2 
+//         return result
+// }
+// let x = addition(number1: 5, number2: 10)
+// print (x)
+
+func  myFunc(number: Int) -> Int  {
+print(number * number)
+return number
+}
+myFunc(number: 2)
