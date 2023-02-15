560287b5b510226eb0390b7156ae33823adbca88
Mohamed Abdulaziz Eisa <83542891+0xnrous@users.noreply.github.com>
2023-02-15 21:40:29 +0200

    Update Dictionary.swift

--- Dictionary/Dictionary.swift
+++ Dictionary/Dictionary.swift
@@ -1,16 +1,16 @@
 var score = ["Eisa": 100 , "Ragab": 100 , "Gerges": 200]
 print (score)
 
-// score ["Eisa"] = 300
-// print(score)
-// score.updateValue(400, forKey: "Ragab")
-// print (score)
+score ["Eisa"] = 300
+print(score)
+score.updateValue(400, forKey: "Ragab")
+print (score)
 
 // // to delete value from dictionary 
 
-// score ["Eisa"]=nil
-// print (score)
-// var boolscore = score.isEmpty
+score ["Eisa"]=nil
+print (score)
+var boolscore = score.isEmpty
 
  // var httpStatusCodes = [200: "Success",
  // 302: "Temporary Redirect",
