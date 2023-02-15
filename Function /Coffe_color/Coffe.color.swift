48070844f89ce0a700fac9a2e3ded2dcc0a1c919
Mohamed Abdulaziz Eisa <83542891+0xnrous@users.noreply.github.com>
2023-02-15 21:41:36 +0200

    Update Coffe.color.swift

--- Function /Coffe_color/Coffe.color.swift
+++ Function /Coffe_color/Coffe.color.swift
@@ -1,13 +1,13 @@
 // make arabic coffe example 
-// func makearabicofee(){
-//                 print ("step 1")
-//                 print ("step 2")
-//                 print ("step 3")
-//                 print ("step 4")
-//                 print ("step 5")
+func makearabicofee(){
+                print ("step 1")
+                print ("step 2")
+                print ("step 3")
+                print ("step 4")
+                print ("step 5")
 
-// }
-// makearabicofee()
+}
+makearabicofee()
 
 // example for MyfavColor
 
