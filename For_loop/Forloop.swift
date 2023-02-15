536dd55fc99425ccea30c1d2496e0ff58a5c318c
Mohamed Abdulaziz Eisa <83542891+0xnrous@users.noreply.github.com>
2023-02-15 21:35:45 +0200

    Add files via upload

---
+++ For_loop/Forloop.swift
@@ -0,0 +1,78 @@
+
+// for loop with lists 
+// var fruits = [ "apple", "orange" , "bannana" ]
+// for _ in fruits {
+//         print (fruits)
+        
+// }
+
+/* 
+output like that:
+
+["apple", "orange", "bannana"]
+["apple", "orange", "bannana"]
+["apple", "orange", "bannana"]
+
+*/
+// var fruits = [ "apple", "orange" , "bannana" ]
+// for items in fruits {
+//         print (items)
+        
+// }
+/*
+output like that:
+apple
+orange
+bannana
+*/
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+// for loop with indexs 
+
+// for index in 1...5{
+//         print ("Hello", index)
+// }
+
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+/*
+method of continue:
+تجعل
+continue جملة التكرار تتوقف وتنتقل لتكرار التعليمات البرمجية للعنصر التالي. يوضح المثال التالي كيف يمكننا استخدام عبارة continue لطباعة الأرقام الفردية فقط في نطاق الأعداد من 1 إلى 10:
+*/
+
+// for index in 1...10 {
+
+// if index % 2 == 0 {
+//  continue 
+        
+// }
+//         print ("\(index), is odd")
+// }
+
+// method of break : all loop statement finished 
+
+// for index in 1...10 {
+
+// if index % 2 == 0 {
+//  break  
+        
+// }
+//         print ("\(index), is odd")
+// }
+
+// for number in 1...10 {
+// if number == 5 {
+
+// continue
+
+// }
+
+// print(number)
+
+// }
+
